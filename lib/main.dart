16d6257c567a665b54c84ef63808ec373f285633
Yo Kobayashi <y.kobayashi@4nonome.com>
2022-07-15 07:01:52 +0900

    プロジェクト名を変更

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mobile Instagram App',
+      title: 'Instagram App Group Tuesday12',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
